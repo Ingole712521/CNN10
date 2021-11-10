7bb265604a168620b2d6f2375d426fcf440ac5f7
Ingole712521 <73941735+Ingole712521@users.noreply.github.com>
2021-11-10 22:41:52 +0530

    Add files via upload

---
+++ UDPC.java
@@ -0,0 +1,30 @@
+package com.Nehal;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+public class UDPC{
+    public final static int SERVICE_PORT = 1235;
+
+    public static void main(String[] args) throws IOException{
+        try{
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] sendingDataBuffer = new byte[1024];
+            byte[] receivingDataBuffer = new byte[1024];
+            String sentence = "Hello from UDP client , its Nehal I have successfully completed the experiment 10 ";
+            sendingDataBuffer = sentence.getBytes();
+            DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer,sendingDataBuffer.length,IPAddress, SERVICE_PORT);
+            clientSocket.send(sendingPacket);
+            DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer,receivingDataBuffer.length);
+            clientSocket.receive(receivingPacket);
+            String receivedData = new String(receivingPacket.getData());
+            System.out.println("Sent from the server: "+receivedData);
+            clientSocket.close();
+        }
+        catch(SocketException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ UDPServer.java
@@ -0,0 +1,33 @@
+package com.Nehal;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+public class UDPServer{
+     public final static int SERVICE_PORT=1235;
+    public static void main(String[] args) throws IOException{
+        try{
+             DatagramSocket serverSocket = new DatagramSocket(SERVICE_PORT);
+            byte[] receivingDataBuffer = new byte[1024];
+            byte[] sendingDataBuffer = new byte[1024];
+            DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
+            System.out.println("Waiting for a client to connect...");
+            serverSocket.receive(inputPacket);
+            String receivedData = new String(inputPacket.getData());
+            System.out.println("Sent from the client: "+receivedData);
+            sendingDataBuffer = receivedData.toUpperCase().getBytes();
+            InetAddress senderAddress = inputPacket.getAddress();
+            int senderPort = inputPacket.getPort();
+            DatagramPacket outputPacket = new DatagramPacket(
+                    sendingDataBuffer, sendingDataBuffer.length,
+                    senderAddress,senderPort
+            );
+            serverSocket.send(outputPacket);
+            serverSocket.close();
+        }
+        catch (SocketException e){
+            e.printStackTrace();
+        }
+    }
+}
